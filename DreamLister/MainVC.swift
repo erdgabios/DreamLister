eefcba4b10edb65bc369b953b5dc16da908affd0
Gabriel Erdelyi <erdgabios@gmail.com>
2016-12-14 20:36:32 +0100

    data model 3

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by G on 12/12/2016.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
