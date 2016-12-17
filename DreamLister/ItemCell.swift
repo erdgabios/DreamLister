a9f14291a00b96dd817378eb8f5837d2763ad66c
Gabriel Erdelyi <erdgabios@gmail.com>
2016-12-17 23:47:16 +0100

    connecting the views

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by G on 17/12/2016.
+//  Copyright © 2016 erdgabios. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
